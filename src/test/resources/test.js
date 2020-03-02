ec1cfe32fab1de1fad7f99bf6ecb582fbb6a1b3c
oranheim <oranheim@gmail.com>
2020-03-02 08:44:11 +0100

    Exploration testing

--- src/test/resources/test.js
+++ src/test/resources/test.js
@@ -2,4 +2,10 @@ function hello() {
     console.log("hello world");
 }
 
+if (typeof Graal != 'undefined') {
+    print(Graal.versionJS);
+    print(Graal.versionGraalVM);
+    print(Graal.isGraalRuntime);
+}
+
 hello();
