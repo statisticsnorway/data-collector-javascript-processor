b64d60c36fff1d8413210db75870ac814e5d579c
oranheim <oranheim@gmail.com>
2020-03-03 06:16:36 +0100

    Fixed broken test

--- src/test/resources/test.js
+++ src/test/resources/test.js
@@ -2,10 +2,4 @@ function hello() {
     console.log("hello world");
 }
 
-if (typeof Graal != 'undefined') {
-    print(Graal.versionJS);
-    print(Graal.versionGraalVM);
-    print(Graal.isGraalRuntime);
-}
-
 hello();
